d63f1dc5e32eed53a357ce589b76a1bc79b8194e
Inky-Moon <asdhyhhf@outlook.com>
2026-08-23 11:42:43 +0800

    Fix KGP unresolved reference error by applying Kotlin plugin declaratively

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,19 +3,11 @@ import org.jetbrains.kotlin.konan.properties.Properties
 
 plugins {
     id("com.android.application")
+    id("org.jetbrains.kotlin.android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-val agpMajorVersion = com.android.Version.ANDROID_GRADLE_PLUGIN_VERSION
-    .substringBefore('.')
-    .toInt()
-val builtInKotlinProperty = providers.gradleProperty("android.builtInKotlin").orNull
-val isBuiltInKotlinEnabled = agpMajorVersion >= 9 &&
-        (builtInKotlinProperty == null || builtInKotlinProperty.toBoolean())
-if (!isBuiltInKotlinEnabled) {
-    apply(plugin = "org.jetbrains.kotlin.android")
-}
 
 android {
     namespace = "com.example.piliplus"
